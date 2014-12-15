786e86170f551ceccc87b2c1cb74b32414246550
muschellij2 <muschellij2@gmail.com>
2014-12-15 14:58:12 -0500

    Added hexdata.

---
+++ R/hexdata.R
@@ -0,0 +1,13 @@
+#' @title hexdata
+#'
+#' @description Hex data to deelete
+#' @name hexdata
+#' @docType data
+#' @aliases hexdata
+#' @usage hexdata
+#' @keywords datasets
+#' @examples
+#' \dontrun{
+#' data(hexdata)
+#' } 
+NULL
